9ab12be4a5acf484dd114e9eb57f35b7a633111f
Dan Kim <dan.kimhaejun@gmail.com>
2019-10-21 17:20:21 +0900

    for test

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.use('/', (req, res) => {
-  res.send('이거는 서버야 알지?')
-})
+app.get('/', (req, res) => {
+  res.send('이거는 서버야 알지?');
+});
+
 app.listen(5001, () => {
   console.log('server on 5001');
-})
+});
