862b4670358f68ce16d004526a8d71933dc93342
Dan Kim <dan.kimhaejun@gmail.com>
2019-10-21 15:46:10 +0900

    create node server

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('hello')
+const express = require('express');
+const app = express();
+
+app.use('/', (req, res) => {
+  res.send('이거는 서버야 알지?')
+})
+app.listen(5000, () => {
+  console.log('server on 5000');
+})
